8280b34bea43e32aa7ed9b53561ae70bfed0dddc
povrozuksn <96878944+povrozuksn@users.noreply.github.com>
2023-09-12 19:09:09 +0400

    Макет главной формы
    Панель авторизации, панель фильтра, панель информации

---
+++ Bookingcom/MainForm.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Bookingcom
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.AuthPanel = new System.Windows.Forms.Panel();
+            this.FiltrPanel = new System.Windows.Forms.Panel();
+            this.InfoPanel = new System.Windows.Forms.Panel();
+            this.AuthButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PassTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CityComboBox = new System.Windows.Forms.ComboBox();
+            this.HotelComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FiltrButton = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.FindButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AuthPanel.SuspendLayout();
+            this.FiltrPanel.SuspendLayout();
+            this.InfoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AuthPanel
+            // 
+            this.AuthPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.AuthPanel.Controls.Add(this.PassTextBox);
+            this.AuthPanel.Controls.Add(this.label2);
+            this.AuthPanel.Controls.Add(this.LoginTextBox);
+            this.AuthPanel.Controls.Add(this.label1);
+            this.AuthPanel.Controls.Add(this.AuthButton);
+            this.AuthPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AuthPanel.Location = new System.Drawing.Point(0, 0);
+            this.AuthPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AuthPanel.Name = "AuthPanel";
+            this.AuthPanel.Size = new System.Drawing.Size(1132, 58);
+            this.AuthPanel.TabIndex = 0;
+            // 
+            // FiltrPanel
+            // 
+            this.FiltrPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.FiltrPanel.Controls.Add(this.FindButton);
+            this.FiltrPanel.Controls.Add(this.label6);
+            this.FiltrPanel.Controls.Add(this.dateTimePicker2);
+            this.FiltrPanel.Controls.Add(this.label5);
+            this.FiltrPanel.Controls.Add(this.dateTimePicker1);
+            this.FiltrPanel.Controls.Add(this.FiltrButton);
+            this.FiltrPanel.Controls.Add(this.HotelComboBox);
+            this.FiltrPanel.Controls.Add(this.label4);
+            this.FiltrPanel.Controls.Add(this.CityComboBox);
+            this.FiltrPanel.Controls.Add(this.label3);
+            this.FiltrPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FiltrPanel.Location = new System.Drawing.Point(0, 58);
+            this.FiltrPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FiltrPanel.Name = "FiltrPanel";
+            this.FiltrPanel.Size = new System.Drawing.Size(1132, 140);
+            this.FiltrPanel.TabIndex = 1;
+            // 
+            // InfoPanel
+            // 
+            this.InfoPanel.Controls.Add(this.pictureBox2);
+            this.InfoPanel.Controls.Add(this.label8);
+            this.InfoPanel.Controls.Add(this.pictureBox1);
+            this.InfoPanel.Controls.Add(this.label7);
+            this.InfoPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InfoPanel.Location = new System.Drawing.Point(0, 206);
+            this.InfoPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.InfoPanel.Name = "InfoPanel";
+            this.InfoPanel.Size = new System.Drawing.Size(1132, 303);
+            this.InfoPanel.TabIndex = 2;
+            // 
+            // AuthButton
+            // 
+            this.AuthButton.Location = new System.Drawing.Point(615, 12);
+            this.AuthButton.Name = "AuthButton";
+            this.AuthButton.Size = new System.Drawing.Size(115, 34);
+            this.AuthButton.TabIndex = 0;
+            this.AuthButton.Text = "Войти";
+            this.AuthButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(101, 14);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(189, 30);
+            this.LoginTextBox.TabIndex = 2;
+            // 
+            // PassTextBox
+            // 
+            this.PassTextBox.Location = new System.Drawing.Point(387, 14);
+            this.PassTextBox.Name = "PassTextBox";
+            this.PassTextBox.Size = new System.Drawing.Size(189, 30);
+            this.PassTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Город";
+            // 
+            // CityComboBox
+            // 
+            this.CityComboBox.FormattingEnabled = true;
+            this.CityComboBox.Items.AddRange(new object[] {
+            "Москва",
+            "Ульяновск",
+            "Самара",
+            "Пенза",
+            "Екатеринбург"});
+            this.CityComboBox.Location = new System.Drawing.Point(128, 59);
+            this.CityComboBox.Name = "CityComboBox";
+            this.CityComboBox.Size = new System.Drawing.Size(185, 32);
+            this.CityComboBox.TabIndex = 4;
+            // 
+            // HotelComboBox
+            // 
+            this.HotelComboBox.FormattingEnabled = true;
+            this.HotelComboBox.Items.AddRange(new object[] {
+            "Венец",
+            "Советская",
+            "Волга"});
+            this.HotelComboBox.Location = new System.Drawing.Point(128, 96);
+            this.HotelComboBox.Name = "HotelComboBox";
+            this.HotelComboBox.Size = new System.Drawing.Size(185, 32);
+            this.HotelComboBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Гостиница";
+            // 
+            // FiltrButton
+            // 
+            this.FiltrButton.Location = new System.Drawing.Point(0, 0);
+            this.FiltrButton.Name = "FiltrButton";
+            this.FiltrButton.Size = new System.Drawing.Size(112, 35);
+            this.FiltrButton.TabIndex = 7;
+            this.FiltrButton.Text = "Фильтр";
+            this.FiltrButton.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(471, 61);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(173, 30);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Дата заезда";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(338, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Дата выезда";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(471, 98);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(173, 30);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // FindButton
+            // 
+            this.FindButton.Location = new System.Drawing.Point(670, 62);
+            this.FindButton.Name = "FindButton";
+            this.FindButton.Size = new System.Drawing.Size(111, 65);
+            this.FindButton.TabIndex = 12;
+            this.FindButton.Text = "Найти";
+            this.FindButton.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Гостиница \"Советская\"";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(342, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(270, 184);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(338, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 24);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Гостиница \"Венец\"";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 509);
+            this.Controls.Add(this.InfoPanel);
+            this.Controls.Add(this.FiltrPanel);
+            this.Controls.Add(this.AuthPanel);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.AuthPanel.ResumeLayout(false);
+            this.AuthPanel.PerformLayout();
+            this.FiltrPanel.ResumeLayout(false);
+            this.FiltrPanel.PerformLayout();
+            this.InfoPanel.ResumeLayout(false);
+            this.InfoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel AuthPanel;
+        private System.Windows.Forms.Panel FiltrPanel;
+        private System.Windows.Forms.Panel InfoPanel;
+        private System.Windows.Forms.TextBox PassTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AuthButton;
+        private System.Windows.Forms.ComboBox HotelComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CityComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button FindButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button FiltrButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
