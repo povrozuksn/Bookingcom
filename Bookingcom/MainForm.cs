b53084e98c92f5d3083429f4a2a08fcb740321be
povser75@outlook.com <povser75@outlook.com>
2023-10-03 19:36:55 +0400

    Подключение к MySQL базе данных

--- Bookingcom/MainForm.cs
+++ Bookingcom/MainForm.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Bookingcom
 {
     public partial class MainForm : Form
     {
+        const string CONNECTION_STRING =
+       "SslMode=none;Server=localhost;Database=bookingcom;port=3306;Uid=root;charset=utf8";
+
         public MainForm()
         {
             InitializeComponent();
+
+            MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT id, name FROM cityes", conn);
+
+            DbDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                string id = reader.GetValue(0).ToString();
+                string name = reader.GetValue(1).ToString();
+            }
+            reader.Close();
+
+            conn.Close();
+
+
         }
 
         private void FiltrButton_Click(object sender, EventArgs e)
